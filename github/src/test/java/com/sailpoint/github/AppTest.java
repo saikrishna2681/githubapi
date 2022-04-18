9c7f6e4541c2513356003a32114e7946243a6b9d
simple.sai220@gmail.com <simple.sai220@gmail.com>
2022-04-18 12:34:04 +0530

    sailpoint task

---
+++ github/src/test/java/com/sailpoint/github/AppTest.java
@@ -0,0 +1,19 @@
+package com.sailpoint.github;
+
+
+
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     * @throws IOException 
+     */
+	//String line;
+
+    
+}
